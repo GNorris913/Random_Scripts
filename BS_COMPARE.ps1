cbb4211658d1c23900261398dad55c94f088a888
Greg Norris <62456168+GNorris913@users.noreply.github.com>
2020-12-22 21:33:47 -0700

    Update BS_COMPARE.ps1

--- BS_COMPARE.ps1
+++ BS_COMPARE.ps1
@@ -1,28 +1,39 @@
 function Compare-BSLog{
 	$log = Get-Content '.\cue_logger.log'; 
-	$csv = Import-Csv -Path '.\NETCUES.csv';
-	$out = $none 
+	$csv = Import-Csv -Path '.\BANG NETCUES.csv';
+	$out = ""; 
 	$csv | ForEach-Object  {
 		if (![string]::IsNullOrEmpty($_.'PROGRAM NAME')){
-			Write-Host "##########          $($_.'PROGRAM NAME')          ##########`r`n";  
+			$xpn = "##########          $($_.'PROGRAM NAME')          ##########`r`n";
+			Write-Host $xpn;
+			$out = $out + $xpn;
+			$xpn = $null;		
 			if(![string]::IsNullOrEmpty($_.'BANG START')){
-				Write-Host "-----   BANG START $($_.'BANG START')   -----";
+				$bsn = "----------          BANG START $($_.'BANG START')          ----------";
+				Write-Host $bsn;
+				$out = $out + $bsn;
 				$cl = $log | Select-String -Pattern "#$($_.'BANG START')@" | Out-String;
 				if(![string]::IsNullOrEmpty($cl)){
 					$cl -split "(>)";
-					Write-Host "$($cl)"
-					$cl = $null
+					$out = $out + $cl;
+					$cl = $null;
 				}
 			};
 			if(![string]::IsNullOrEmpty($_.'BANG END')){
-				Write-Host "-----   BANG END $($_.'BANG END')   -----";
+				$ben = "----------          BANG END $($_.'BANG END')          ----------";
+				Write-Host $ben;
 				$cl = $log | Select-String -Pattern "#$($_.'BANG END')@" | Out-String;
 				if(![string]::IsNullOrEmpty($cl)){
 					$cl -split "(>)";
-					Write-Host "$($cl)";
-					$cl = $null
+					$out = $out + $ben;
+					$out = $out + $cl;
+					$cl = $null;
 				}
 			}
 		}
-	}
+	};
+	foreach ($line in $out){
+		Write-Host $line;
+		Add-Content -Path ".\sat_svcs.txt" -Value $line
+	};
 }
